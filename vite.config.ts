97058dbd96b0aa122aa054db0b5b78aabee27628
Borna Šoštarić <sostaric.borna.dev@hotmail.com>
2025-02-27 17:05:46 +0100

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(() => {
+	let base = {};
+	if (process.env.NODE_ENV === "production") {
+		base = {
+			base: "/db-docs/",
+		};
+	}
+
+	return {
+		...base,
+		plugins: [react()],
+	};
+});
