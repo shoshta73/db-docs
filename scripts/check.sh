3006ed49da141235a3f87fb68849d69a9106e696
Borna Šoštarić <sostaric.borna.dev@hotmail.com>
2025-02-27 17:36:59 +0100

    add check script

---
+++ scripts/check.sh
@@ -0,0 +1,18 @@
+if pnpm -v ; then
+  if [ ! -d node_modules ]; then
+    pnpm i
+  fi
+
+  if [ ! pnpm run check:ci ]; then
+    if [ ! pnpm run format:ci]; then
+      pnpm run format
+    fi
+
+    if [ ! pnpm run lint:ci ]; then
+      pnpm run lint
+    fi
+
+    git add .
+    git commit -m "autofix: biome check"
+  fi
+fi
